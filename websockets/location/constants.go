35bcb75d1f8d7273dac216a346558cbebd87cb00
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-04-16 02:06:37 +0100

    add const for location update in seconds

--- websockets/location/constants.go
+++ websockets/location/constants.go
@@ -3,10 +3,11 @@ package location
 import "time"
 
 const (
-	UpdateCooldown = 3 * time.Second
-	Timeout        = 3 * UpdateCooldown
-	PingCooldown   = 1 * time.Second
+	UpdateCooldown          = 3 * time.Second
+	UpdateCooldownInSeconds = 3
+	Timeout                 = 3 * UpdateCooldown
+	PingCooldown            = 1 * time.Second
 
 	UpdateLocation = "UPDATE_LOCATION"
-	Gyms = "GYMS"
+	Gyms           = "GYMS"
 )
