a66ea19330c99f13b713e44d801f99225ed331f3
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-04-16 18:51:51 +0100

    add notification msg types

---
+++ messages/notifications/types.go
@@ -0,0 +1,49 @@
+package notifications
+
+import (
+	"encoding/json"
+	"github.com/NOVAPokemon/utils"
+	"github.com/NOVAPokemon/utils/messages"
+	ws "github.com/NOVAPokemon/utils/websockets"
+	"github.com/NOVAPokemon/utils/websockets/trades"
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	NOTIFICATION = "NOTIFICATION"
+)
+
+// Notification
+type NotificationMessage struct {
+	Notification utils.Notification
+	messages.MessageWithId
+}
+
+func (nMsg NotificationMessage) SerializeToWSMessage() *ws.Message {
+	msgJson, err := json.Marshal(nMsg)
+	if err != nil {
+		log.Error(err)
+		return nil
+	}
+
+	return &ws.Message{
+		MsgType: NOTIFICATION,
+		MsgArgs: []string{string(msgJson)},
+	}
+}
+
+func Deserialize(msg *ws.Message) messages.Serializable {
+	switch msg.MsgType {
+	case trades.START:
+		var notificationMsg NotificationMessage
+		err := json.Unmarshal([]byte(msg.MsgArgs[0]), &notificationMsg)
+		if err != nil {
+			log.Error(err)
+			return nil
+		}
+
+		return &notificationMsg
+	default:
+		return nil
+	}
+}
