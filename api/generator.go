7be7d750d63b62efc67585c2e81df35e329d3aeb
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-04-21 20:00:03 +0100

    remove wild pokemons from generator to location

--- api/generator.go
+++ api/generator.go
@@ -1,7 +1,5 @@
 package api
 
-const CatchWildPokemonPath = "/catch"
 const GenerateRaidBossPath = "/boss"
 
-var CatchWildPokemonRoute = CatchWildPokemonPath
 var GenerateRaidBossRoute = GenerateRaidBossPath
--- api/location.go
+++ api/location.go
@@ -1,7 +1,19 @@
 package api
 
+import (
+	"github.com/NOVAPokemon/utils/items"
+	"github.com/NOVAPokemon/utils/pokemons"
+)
+
 const UserLocationPath = "/user_location"
 const GymLocationPath = "/gym_location"
+const CatchWildPokemonPath = "/catch"
 
 var UserLocationRoute = UserLocationPath
 var GymLocationRoute = GymLocationPath
+var CatchWildPokemonRoute = CatchWildPokemonPath
+
+type CatchWildPokemonRequest struct {
+	Pokeball items.Item
+	Pokemon  pokemons.Pokemon
+}
