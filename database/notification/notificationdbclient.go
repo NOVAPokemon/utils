9b5b011be3e68c0db5cd4b9109a7a25526e46a84
Bruno Anjos <banjos@localhost.localdomain>
2020-04-30 18:50:14 +0100

    refactor notification errors

--- database/notification/notificationdbclient.go
+++ database/notification/notificationdbclient.go
@@ -2,7 +2,6 @@ package notification
 
 import (
 	"context"
-	"errors"
 	"github.com/NOVAPokemon/utils"
 	databaseUtils "github.com/NOVAPokemon/utils/database"
 	log "github.com/sirupsen/logrus"
@@ -18,60 +17,6 @@ const databaseName = "NOVAPokemonDB"
 const collectionName = "Notifications"
 
 var dbClient databaseUtils.DBClient
-
-func GetNotificationsByUsername(username string) (*utils.Notification, error) {
-	var ctx = dbClient.Ctx
-	var collection = dbClient.Collection
-	var result utils.Notification
-
-	filter := bson.M{"username": username}
-	err := collection.FindOne(*ctx, filter).Decode(&result)
-
-	if err != nil {
-		log.Error(err)
-		return nil, err
-	}
-
-	return &result, nil
-}
-
-func AddNotification(notification utils.Notification) error {
-	ctx := dbClient.Ctx
-	collection := dbClient.Collection
-
-	notificationId := primitive.NewObjectID()
-	notification.Id = notificationId
-
-	_, err := collection.InsertOne(*ctx, notification)
-
-	if err != nil {
-		log.Error(err)
-		return err
-	}
-
-	log.Infof("Added notification %s to user: %s", notification.Id, notification.Username)
-	return err
-}
-
-func RemoveNotification(id primitive.ObjectID) error {
-	ctx := dbClient.Ctx
-	collection := dbClient.Collection
-
-	res, err := collection.DeleteOne(*ctx, id)
-
-	if err != nil {
-		log.Error(err)
-		return err
-	}
-
-	if res.DeletedCount < 1 {
-		log.Error("tried to remove notification that didnt exist")
-		return errors.New("")
-	}
-
-	log.Infof("Removed notification %s", id)
-	return nil
-}
 
 func init() {
 	url, exists := os.LookupEnv(utils.MongoEnvVar)
@@ -104,4 +49,54 @@ func init() {
 
 	_, _ = collection.Indexes().CreateOne(ctx, index)
 	dbClient = databaseUtils.DBClient{Client: client, Ctx: &ctx, Collection: collection}
+}
+
+func GetNotificationsByUsername(username string) (*utils.Notification, error) {
+	var ctx = dbClient.Ctx
+	var collection = dbClient.Collection
+	var result utils.Notification
+
+	filter := bson.M{"username": username}
+	err := collection.FindOne(*ctx, filter).Decode(&result)
+
+	if err != nil {
+		return nil, wrapGetNotificationError(err, username)
+	}
+
+	return &result, nil
+}
+
+func AddNotification(notification utils.Notification) error {
+	ctx := dbClient.Ctx
+	collection := dbClient.Collection
+
+	notificationId := primitive.NewObjectID()
+	notification.Id = notificationId
+
+	_, err := collection.InsertOne(*ctx, notification)
+
+	if err != nil {
+		return wrapAddNotificationError(err, notification.Username)
+	}
+
+	log.Infof("Added notification %s to user: %s", notification.Id, notification.Username)
+	return err
+}
+
+func RemoveNotification(id primitive.ObjectID) error {
+	ctx := dbClient.Ctx
+	collection := dbClient.Collection
+
+	res, err := collection.DeleteOne(*ctx, id)
+
+	if err != nil {
+		return wrapRemoveNotificationError(err, id.Hex())
+	}
+
+	if res.DeletedCount < 1 {
+		return wrapRemoveNotificationError(errorNotificationNotFound, id.Hex())
+	}
+
+	log.Infof("Removed notification %s", id)
+	return nil
 }
