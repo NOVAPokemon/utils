eabf0aed087fe0b8eee208c5b935dab6f3289ac3
Bruno Anjos <banjos@localhost.localdomain>
2020-04-30 18:23:17 +0100

    reorder methods, add new error wrappers

--- database/user/userdbclient.go
+++ database/user/userdbclient.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"context"
+	"errors"
 	"github.com/NOVAPokemon/utils"
 	databaseUtils "github.com/NOVAPokemon/utils/database"
 	log "github.com/sirupsen/logrus"
@@ -16,136 +17,6 @@ const collectionName = "Users"
 
 var dbClient databaseUtils.DBClient
 
-func GetAllUsers() []utils.User {
-
-	var ctx = dbClient.Ctx
-	var collection = dbClient.Collection
-	var results []utils.User
-
-	cur, err := collection.Find(*ctx, bson.M{})
-
-	if err != nil {
-		log.Error(err)
-	}
-
-	defer databaseUtils.CloseCursor(cur, ctx)
-	for cur.Next(*ctx) {
-		var result utils.User
-		err := cur.Decode(&result)
-		if err != nil {
-			log.Error(err)
-		} else {
-			results = append(results, result)
-		}
-	}
-
-	if err := cur.Err(); err != nil {
-		log.Error(err)
-	}
-	return results
-}
-
-func AddUser(user *utils.User) (error, string) {
-
-	var ctx = dbClient.Ctx
-	var collection = dbClient.Collection
-	_, err := collection.InsertOne(*ctx, *user)
-
-	if err != nil {
-		log.Error(err)
-		return err, ""
-	}
-
-	return nil, user.Username
-}
-
-func CheckIfUserExists(username string) (bool, error) {
-
-	var ctx = dbClient.Ctx
-	var collection = dbClient.Collection
-	var limit int64 = 1
-
-	filter := bson.M{"username": username}
-	opts := options.CountOptions{Limit: &limit}
-
-	count, err := collection.CountDocuments(*ctx, filter, &opts)
-
-	if err != nil {
-		log.Error(err)
-		return false, err
-	}
-
-	return count > 0, nil
-}
-
-func GetUserByUsername(username string) (error, *utils.User) {
-	var ctx = dbClient.Ctx
-	var collection = dbClient.Collection
-	result := &utils.User{}
-
-	filter := bson.M{"username": username}
-	err := collection.FindOne(*ctx, filter).Decode(result)
-
-	if err != nil {
-		log.Error(err)
-		return err, nil
-	}
-
-	return nil, result
-}
-
-func UpdateUser(username string, user *utils.User) (error, *utils.User) {
-
-	ctx := dbClient.Ctx
-	collection := dbClient.Collection
-	filter := bson.M{"username": username}
-	user.Username = username
-
-	res, err := collection.ReplaceOne(*ctx, filter, *user)
-
-	if err != nil {
-		log.Error(err)
-		return err, nil
-	}
-
-	if res.MatchedCount > 0 {
-		log.Infof("Updated User %+v", username)
-	} else {
-		log.Errorf("Update User failed because no user matched %+v", username)
-	}
-
-	return nil, user
-
-}
-
-func DeleteUser(username string) error {
-	var ctx = dbClient.Ctx
-	var collection = dbClient.Collection
-	filter := bson.M{"username": username}
-
-	_, err := collection.DeleteOne(*ctx, filter)
-
-	if err != nil {
-		log.Error(err)
-	}
-
-	return err
-}
-
-func removeAll() error {
-	var ctx = dbClient.Ctx
-	var collection = dbClient.Collection
-	filter := bson.M{}
-
-	_, err := collection.DeleteMany(*ctx, filter)
-
-	if err != nil {
-		log.Error(err)
-	}
-
-	return err
-}
-
 func init() {
 	url, exists := os.LookupEnv(utils.MongoEnvVar)
 
@@ -154,14 +25,12 @@ func init() {
 	}
 
 	client, err := mongo.NewClient(options.Client().ApplyURI(url))
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	ctx := context.Background()
 	err = client.Connect(ctx)
-
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -177,4 +46,117 @@ func init() {
 
 	_, _ = collection.Indexes().CreateOne(ctx, index)
 	dbClient = databaseUtils.DBClient{Client: client, Ctx: &ctx, Collection: collection}
+}
+
+func GetAllUsers() ([]utils.User, error) {
+	var ctx = dbClient.Ctx
+	var collection = dbClient.Collection
+	var results []utils.User
+
+	cur, err := collection.Find(*ctx, bson.M{})
+	if err != nil {
+		return nil, wrapGetAllUsersError(err)
+	}
+
+	defer databaseUtils.CloseCursor(cur, ctx)
+	for cur.Next(*ctx) {
+		var result utils.User
+		err := cur.Decode(&result)
+		if err != nil {
+			return nil, wrapGetAllUsersError(err)
+		} else {
+			results = append(results, result)
+		}
+	}
+
+	if err := cur.Err(); err != nil {
+		return nil, wrapGetAllUsersError(err)
+	}
+	return results, nil
+}
+
+func AddUser(user *utils.User) (string, error) {
+	var ctx = dbClient.Ctx
+	var collection = dbClient.Collection
+	_, err := collection.InsertOne(*ctx, *user)
+
+	if err != nil {
+		return "", wrapAddUserError(err, user.Username)
+	}
+
+	return user.Username, nil
+}
+
+func CheckIfUserExists(username string) (bool, error) {
+	var ctx = dbClient.Ctx
+	var collection = dbClient.Collection
+	var limit int64 = 1
+
+	filter := bson.M{"username": username}
+	opts := options.CountOptions{Limit: &limit}
+
+	count, err := collection.CountDocuments(*ctx, filter, &opts)
+	if err != nil {
+		return false, wrapUserExistsError(err, username)
+	}
+
+	return count > 0, nil
+}
+
+func GetUserByUsername(username string) (*utils.User, error) {
+	var ctx = dbClient.Ctx
+	var collection = dbClient.Collection
+
+	result := &utils.User{}
+	filter := bson.M{"username": username}
+
+	err := collection.FindOne(*ctx, filter).Decode(result)
+	if err != nil {
+		return nil, wrapGetUserError(err, username)
+	}
+
+	return result, nil
+}
+
+func UpdateUser(username string, user *utils.User) (*utils.User, error) {
+
+	ctx := dbClient.Ctx
+	collection := dbClient.Collection
+	filter := bson.M{"username": username}
+	user.Username = username
+
+	res, err := collection.ReplaceOne(*ctx, filter, *user)
+
+	if err != nil {
+		return nil, wrapUpdateUserError(err, username)
+	}
+
+	if res.MatchedCount > 0 {
+		log.Infof("Updated User %+v", username)
+	} else {
+		return nil, wrapUpdateUserError(errors.New("no user found"), username)
+	}
+
+	return user, nil
+
+}
+
+func DeleteUser(username string) error {
+	var ctx = dbClient.Ctx
+	var collection = dbClient.Collection
+	filter := bson.M{"username": username}
+
+	_, err := collection.DeleteOne(*ctx, filter)
+
+	return wrapDeleteUserError(err, username)
+}
+
+func removeAll() error {
+	var ctx = dbClient.Ctx
+	var collection = dbClient.Collection
+	filter := bson.M{}
+
+	_, err := collection.DeleteMany(*ctx, filter)
+
+	return wrapDeleteAllUsersError(err)
 }
