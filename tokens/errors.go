216a64e8fa6d3c37c0e87d2435b77883bb0383c3
Bruno Anjos <banjos@localhost.localdomain>
2020-05-05 12:25:39 +0100

    refactor token error names

--- tokens/errors.go
+++ tokens/errors.go
@@ -3,7 +3,7 @@ package tokens
 import "github.com/pkg/errors"
 
 var (
-	ErrorPokemonTokens = errors.New("invalid pokemon hashes")
-	ErrorStatsToken    = errors.New("invalid stats token")
-	ErrorItemsToken    = errors.New("invalid items token")
+	ErrorInvalidPokemonTokens = errors.New("invalid pokemon hashes")
+	ErrorInvalidStatsToken    = errors.New("invalid stats token")
+	ErrorInvalidItemsToken    = errors.New("invalid items token")
 )
