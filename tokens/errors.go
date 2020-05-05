6671dad12060117ba213dd49e721197762a0946b
Bruno Anjos <banjos@localhost.localdomain>
2020-05-05 17:30:26 +0100

    add no pokemon tokens error

--- tokens/errors.go
+++ tokens/errors.go
@@ -3,6 +3,8 @@ package tokens
 import "github.com/pkg/errors"
 
 var (
+	ErrorNoPokemonTokens = errors.New("no pokemon tokens in header")
+
 	ErrorInvalidPokemonTokens = errors.New("invalid pokemon hashes")
 	ErrorInvalidStatsToken    = errors.New("invalid stats token")
 	ErrorInvalidItemsToken    = errors.New("invalid items token")
