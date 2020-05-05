bf1f01280ae7b75e06c0e70c6c1b1e9921b2e157
Bruno Anjos <banjos@localhost.localdomain>
2020-05-05 16:00:12 +0100

    add dialing error to websocket errors

--- websockets/errors.go
+++ websockets/errors.go
@@ -8,11 +8,12 @@ import (
 const (
 	errorUpgradeConnection = "error upgrading connection to websocket"
 	errorClosingConnection = "error closing connection"
-	errorWritingMessage = "error writing message"
-	errorReadingMessage = "error reading message"
+	errorWritingMessage    = "error writing message"
+	errorReadingMessage    = "error reading message"
 
 	errorParsingMessageFormat = "error parsing message from websocket, %s"
 	errorInvalidMsgTypeFormat = "error unsupported msg type %s"
+	errorDialingMessageFormat = "error dialing %s"
 )
 
 var (
@@ -41,8 +42,11 @@ func WrapMsgParsingError(err error, msgString string) error {
 	return errors.Wrap(err, fmt.Sprintf(errorParsingMessageFormat, msgString))
 }
 
+func WrapDialingError(err error, url string) error {
+	return errors.Wrap(err, fmt.Sprintf(errorDialingMessageFormat, url))
+}
+
 // Error builders
 func NewInvalidMsgTypeError(msgType string) error {
 	return errors.New(fmt.Sprintf(errorInvalidMsgTypeFormat, msgType))
 }
-
