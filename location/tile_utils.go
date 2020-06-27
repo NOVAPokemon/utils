4c5b33fdef2745bfe9e41e652d282dc038cde50b
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-06-27 02:42:33 +0100

    fix withinBounds verification

--- location/tile_utils.go
+++ location/tile_utils.go
@@ -1,18 +1,34 @@
 package location
 
 import (
+	"fmt"
+
 	"github.com/NOVAPokemon/utils"
 	"github.com/golang/geo/r2"
 )
 
 func IsWithinBounds(location utils.Location, topLeft utils.Location, botRight utils.Location) bool {
-	if location.Longitude >= botRight.Longitude || location.Longitude <= topLeft.Longitude {
-		return false
+	topLeftPoint := r2.Point{
+		X: topLeft.Longitude,
+		Y: topLeft.Latitude,
 	}
-	if location.Latitude <= botRight.Latitude || location.Latitude >= topLeft.Latitude {
-		return false
+
+	botRightPoint := r2.Point{
+		X: botRight.Longitude,
+		Y: botRight.Latitude,
 	}
-	return true
+
+	boundsRect := r2.RectFromPoints(topLeftPoint, botRightPoint)
+
+	locPoint := r2.Point{
+		X: location.Longitude,
+		Y: location.Latitude,
+	}
+
+	fmt.Printf("checking if {%f, %f} in {%f, %f}{%f, %f}\n", location.Longitude, location.Latitude,
+		topLeft.Longitude, topLeft.Latitude, botRight.Longitude, botRight.Latitude)
+
+	return boundsRect.ContainsPoint(locPoint)
 }
 
 func LocationsToRect(topLeft, botRight utils.Location) r2.Rect {
