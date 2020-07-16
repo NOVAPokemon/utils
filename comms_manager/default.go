4d811689915f7906e54f179ff74a6465e9f31ea4
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-07-16 11:29:42 +0100

    add default comms manager

--- comms_manager/default.go
+++ comms_manager/default.go
@@ -2,14 +2,31 @@ package comms_manager
 
 import (
 	"net/http"
+
+	"github.com/NOVAPokemon/utils/websockets"
+	"github.com/gorilla/websocket"
 )
 
 type DefaultCommsManager struct{}
 
-func (w *DefaultCommsManager) WriteMessageToConn(conn *WsConnWithServerName, msgType int, data []byte) error {
+func (d *DefaultCommsManager) WriteTextMessageToConn(conn *websocket.Conn,
+	serializable websockets.Serializable) error {
+	return conn.WriteMessage(websocket.TextMessage, []byte(serializable.SerializeToWSMessage().Serialize()))
+}
+
+func (d *DefaultCommsManager) WriteNonTextMessageToConn(conn *websocket.Conn, msgType int, data []byte) error {
 	return conn.WriteMessage(msgType, data)
 }
 
-func (w *DefaultCommsManager) DoHTTPRequest(client *http.Client, req *http.Request) (*http.Response, error) {
+func (d *DefaultCommsManager) ReadTextMessageFromConn(conn *websocket.Conn) ([]byte, error) {
+	_, p, err := conn.ReadMessage()
+	return p, err
+}
+
+func (d *DefaultCommsManager) DoHTTPRequest(client *http.Client, req *http.Request) (*http.Response, error) {
 	return client.Do(req)
+}
+
+func (d *DefaultCommsManager) HTTPRequestInterceptor(next http.Handler) http.Handler {
+	return next
 }
