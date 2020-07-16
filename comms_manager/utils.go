18157eed061dbf4dd0ad44a80753e71726a5dcaa
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-07-16 11:30:15 +0100

    add comms manager interface

--- comms_manager/utils.go
+++ comms_manager/utils.go
@@ -3,15 +3,14 @@ package comms_manager
 import (
 	"net/http"
 
+	"github.com/NOVAPokemon/utils/websockets"
 	"github.com/gorilla/websocket"
 )
 
-type WsConnWithServerName struct {
-	websocket.Conn
-	ServerName string
-}
-
 type CommunicationManager interface {
-	WriteMessageToConn(conn *WsConnWithServerName, msgType int, data []byte) error
+	WriteTextMessageToConn(conn *websocket.Conn, serializable websockets.Serializable) error
+	WriteNonTextMessageToConn(conn *websocket.Conn, msgType int, data []byte) error
+	ReadTextMessageFromConn(conn *websocket.Conn) ([]byte, error)
 	DoHTTPRequest(client *http.Client, req *http.Request) (*http.Response, error)
+	HTTPRequestInterceptor(next http.Handler) http.Handler
 }
