13462d190b8cbc90e8f833089a504a29a3bfb1ee
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-07-16 11:29:51 +0100

    add delayed comms manager

--- comms_manager/delayed.go
+++ comms_manager/delayed.go
@@ -1,78 +1,67 @@
 package comms_manager
 
 import (
-	"fmt"
 	"net/http"
-	"strings"
 	"time"
 
-	"github.com/golang/geo/s2"
+	"github.com/NOVAPokemon/utils/websockets"
+	"github.com/gorilla/websocket"
 )
 
-type LocationLatency struct {
-	locationName       string
-	directLatency      int
-	subLocationLatency int
-	subLocations       []LocationLatency
-}
+const (
+	locationTagKey = "Location_tag"
+)
+
+type DelaysMatrixType = map[string]map[string]int
 
 type DelayedCommsManager struct {
-	locations           []LocationLatency
-	myLocation          string
-	positionInitialized bool
+	LocationTag  string
+	DelaysMatrix *DelaysMatrixType
 }
 
-func (w *DelayedCommsManager) setLocation(location s2.LatLng) {
-	w.myLocation = location
-	if !w.positionInitialized {
-		w.positionInitialized = true
-	}
+func (d *DelayedCommsManager) WriteTextMessageToConn(conn *websocket.Conn,
+	serializable websockets.Serializable) error {
+
+	msg := websockets.TaggedMessage{
+		LocationTag: d.LocationTag,
+		MsgBytes:    []byte(serializable.SerializeToWSMessage().Serialize()),
+	}.SerializeToWSMessage().Serialize()
+
+	return conn.WriteMessage(websocket.TextMessage, []byte(msg))
 }
 
-// For now we only take into account two levels (Datacenter and directly connected edge nodes)
-func (w *DelayedCommsManager) getDelay() time.Duration {
-	found := false
-	delayToApply := 0
-
-	closest := -1
-	delayToApply := 0
-
-	for _, location := range w.locations {
-		location.
-		for _, subLocation := range location.subLocations {
-			if subLocation.directLatency + location.directLatency < currClosest {
-
-			}
-		}
-	}
-
-	for serverName, delay := range w.delayConfigs.delays {
-		if strings.Contains(serverNameToFind, serverName) {
-			found = true
-			delayToApply = delay
-		}
-	}
-	if !found {
-		if defaultDelay, ok := w.delayConfigs.delays[defaultDelayField]; ok {
-			if !ok {
-				panic(fmt.Sprintf("could not find delay for %s: %+v", serverNameToFind, w.delayConfigs.delays))
-			}
-			delayToApply = defaultDelay
-		}
-	}
-
-	return time.Duration(delayToApply) * time.Millisecond
-}
-
-func (w *DelayedCommsManager) WriteMessageToConn(conn *WsConnWithServerName, msgType int, data []byte) error {
-	delay := w.getDelayFromServerName(conn.ServerName)
-
-	time.Sleep(delay * time.Millisecond)
+func (d *DelayedCommsManager) WriteNonTextMessageToConn(conn *websocket.Conn, msgType int, data []byte) error {
 	return conn.WriteMessage(msgType, data)
 }
 
-func (w *DelayedCommsManager) DoHTTPRequest(client *http.Client, req *http.Request) (*http.Response, error) {
-	delay := w.getDelayFromServerName(req.URL.Host)
-	time.Sleep(delay * time.Millisecond)
+func (d *DelayedCommsManager) ReadTextMessageFromConn(conn *websocket.Conn) ([]byte, error) {
+	_, p, err := conn.ReadMessage()
+	if err != nil {
+		return nil, err
+	}
+
+	taggedMessage, err := websockets.DeserializeTaggedMessage(p)
+	if err != nil {
+		panic(err)
+	}
+
+	requesterLocationTag := taggedMessage.LocationTag
+	sleepDuration := time.Duration((*d.DelaysMatrix)[requesterLocationTag][d.LocationTag]) * time.Millisecond
+	time.Sleep(sleepDuration)
+
+	return taggedMessage.MsgBytes, nil
+}
+
+func (d *DelayedCommsManager) DoHTTPRequest(client *http.Client, req *http.Request) (*http.Response, error) {
+	req.Header.Set(locationTagKey, d.LocationTag)
 	return client.Do(req)
+}
+
+func (d *DelayedCommsManager) HTTPRequestInterceptor(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requesterLocationTag := r.Header.Get(locationTagKey)
+		sleepDuration := time.Duration((*d.DelaysMatrix)[requesterLocationTag][d.LocationTag]) * time.Millisecond
+		time.Sleep(sleepDuration)
+		next.ServeHTTP(w, r)
+	})
 }
