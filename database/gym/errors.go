4b24243a51e550b54daa966248fcea370dcd102e
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-07-21 11:46:41 +0100

    remove unused function

--- database/gym/errors.go
+++ database/gym/errors.go
@@ -8,16 +8,11 @@ import (
 
 const (
 	errorGetServerConfig    = "error getting server %s configs"
-	errorGetConfig          = "error getting configs"
 	errorUpdateServerConfig = "error updating server %s configs"
 )
 
 func wrapUpdateServerConfig(err error, serverName string) error {
 	return errors.Wrap(err, fmt.Sprintf(errorUpdateServerConfig, serverName))
-}
-
-func wrapGetConfig(err error) error {
-	return errors.Wrap(err, errorGetConfig)
 }
 
 func wrapGetServerConfig(err error, serverName string) error {
